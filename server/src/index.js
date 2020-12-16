01124813faf6433095c2eb96ad77f5b15634a3cf
ElBarbaro007 <casystem0@gmail.com>
2020-12-15 20:56:59 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,33 +1,7 @@
 'use strict';
 
 const express = require('express');
-const path = require('path');
-
-// Constants
-const PORT = process.env.PORT || 8080;
-const HOST = '0.0.0.0';
-
-const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
-
-// App
-const app = express();
-
-// Static files
-app.use(express.static(CLIENT_BUILD_PATH));
-
-// API
-app.get('/api', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  let data = {
-    message: 'Proyecto INTEGRACION CONTINUA GIT + Docker + Jenkins  + AZURE'
-  };
-  res.send(JSON.stringify(data, null, 2));
-});
-
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+const http = require('http');
+const path = require('path');let app = express();app.use(express.static(path.join(__dirname, 'build')));const port = process.env.PORT || '8080';
+app.set('port', port);const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
